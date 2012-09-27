2c396f25aace767e8cdf28bda8a2622a9aeb8304
Martin Dybdal <dybber@dybber.dk>
2012-09-27 14:44:15 +0200

    Rolf's American Put Option pricer (R version) added to survey

---
+++ surveying/binomial/Instances/R/AmrPut.R
@@ -0,0 +1,43 @@
+# Option pricer code from Rolf Poulsen
+AmericanPutOptionPricer <- function(S0, r, alpha, sigma, expiry, n){
+  strike<-S0*exp(r*expiry)
+  dt<-expiry/n
+  u<-exp(alpha*dt+sigma*sqrt(dt)); d<-exp(alpha*dt-sigma*sqrt(dt))
+  R<-exp(r*dt)
+  q<-(R-d)/(u-d)
+  qUR<-q/R; qDR<-(1-q)/R
+  u.pow<-u^(0:n); d.pow<-d^(0:n)
+
+  St<-S0*u.pow[1:(n+1)]*d.pow[(n+1):1]
+  put<-pmax(strike - St,0)
+
+  for (i in n:1) {
+    St<-S0*u.pow[1:i]*d.pow[i:1]
+    put[1:i]<-pmax(strike-St,(qUR*put[2:(i+1)]+qDR*put[1:i]))
+  }
+  
+  return(put[1])
+}
+
+# Default model parameters
+S0<-100;
+r<-0.03;
+alpha<-0.07;
+sigma<-0.20
+expiry<-1;
+
+cat("OK\n")
+
+while(TRUE) {
+  str = readLines(con="stdin", 1)
+  if(str == "EXIT")
+    break;
+
+  iterations = as.numeric(str)
+  n <- expiry * iterations;
+  result = AmericanPutOptionPricer(S0, r, alpha, sigma, expiry, n)
+
+  cat("RESULT ")
+  cat(result)
+  cat("\n")
+}
