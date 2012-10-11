f14a73a1266d672f77e6022f70eceecb9b5f5ffa
Martin Dybdal <dybber@dybber.dk>
2012-10-11 09:16:01 +0200

    Accelerate pricer runs

--- surveying/binomial/Instances/Accelerate/Main.hs
+++ surveying/binomial/Instances/Accelerate/Main.hs
@@ -4,8 +4,8 @@ import American
 import BinomialBenchmark.Main(runTest)
 
 import qualified Data.Array.Accelerate as A
-import Data.Array.Accelerate.Interpreter (run)
---import Data.Array.Accelerate.CUDA (run)
+--import Data.Array.Accelerate.Interpreter (run)
+import Data.Array.Accelerate.CUDA (run)
 
 main = runTest go
   where
