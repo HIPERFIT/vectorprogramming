ffc5635b92afaf6337a24bf38e4482d8e2f957d6
Martin Dybdal <dybber@dybber.dk>
2012-10-16 16:29:23 +0200

    Corrected CUDA pricer. Eliminated rounding errors and now also works with expiry = 128 and expiry = 256

--- surveying/binomial/Instances/Cuda/src-cpp/binomialOptions.cpp
+++ surveying/binomial/Instances/Cuda/src-cpp/binomialOptions.cpp
@@ -26,22 +26,9 @@
 // Process an array of OptN options on GPU
 ////////////////////////////////////////////////////////////////////////////////
 extern "C" void binomialOptions_SM13(
-    float *callValue,
-    TOptionData  *optionData,
-    int optN
+    double *callValue,
+    TOptionData  optionData
 );
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Helper function, returning uniformly distributed
-// random float in [low, high] range
-////////////////////////////////////////////////////////////////////////////////
-float randData(float low, float high)
-{
-    float t = (float)rand() / (float)RAND_MAX;
-    return (1.0f - t) * low + t * high;
-}
-
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -49,7 +36,6 @@ float randData(float low, float high)
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-    const unsigned int OPT_N_MAX = 1; // 512
 
     setlinebuf(stdout);
 
@@ -58,18 +44,17 @@ int main(int argc, char **argv)
     if (devID < 0)
     {
         printf("No CUDA Capable devices found, exiting...\n");
-        //shrQAFinishExit(argc, (const char **)argv, QA_WAIVED);
     }
 
     checkCudaErrors(cudaGetDevice(&devID));
+
     cudaDeviceProp deviceProp;
     checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
 
-    const int OPT_N = OPT_N_MAX;
-    TOptionData optionData[OPT_N];
+    TOptionData optionData;
 
     double errorVal;
-    float callValueGPU[OPT_N_MAX];
+    double callValueGPU;
     int i;
 
     char inBuf[200]; // ridiculously large input buffer.
@@ -94,39 +79,21 @@ int main(int argc, char **argv)
         }
       }
 
-      /* model parameters from the Vector version in Haskell:
-      strike = 100
-      bankDays = 252
-      s0 = 100
-      r = 0.03; alpha = 0.07; sigma = 0.20
-
-      n = expiry*bankDays
-      dt = fromIntegral expiry/fromIntegral n
-      u = exp(alpha*dt+sigma*sqrt dt)
-      d = exp(alpha*dt-sigma*sqrt dt)
-      stepR = exp(r*dt)
-      q = (stepR-d)/(u-d)
-      qUR = q/stepR; qDR = (1-q)/stepR
-      */
-
-      for (i = 0; i < OPT_N; i++)
-      {
-          optionData[i].S_0 = 100.0f; // randData(5.0f, 30.0f);
-          optionData[i].strike = 100.0f; // randData(1.0f, 100.0f);
-          optionData[i].r = 0.03f;
-          optionData[i].bankDays = 256;
-          optionData[i].expiry = expiry;
-          optionData[i].sigma = 0.20f;
-          optionData[i].alpha = 0.07f;
-      }
+      optionData.S_0 = 100.0f; // randData(5.0f, 30.0f);
+      optionData.strike = 100.0f; // randData(1.0f, 100.0f);
+      optionData.r = 0.03f;
+      optionData.bankDays = 256;
+      optionData.expiry = expiry;
+      optionData.sigma = 0.20f;
+      optionData.alpha = 0.07f;
 
       checkCudaErrors(cudaDeviceSynchronize());
 
-      binomialOptions_SM13(callValueGPU, optionData, OPT_N);
+      binomialOptions_SM13(&callValueGPU, optionData);
 
       checkCudaErrors(cudaDeviceSynchronize());
 
-      printf("RESULT %f\n",callValueGPU[0]);
+      printf("RESULT %f\n",callValueGPU);
     }
 
     cudaDeviceReset();
