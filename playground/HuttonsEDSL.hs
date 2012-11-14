0b5db65e42d5419ac494f00c07745cb1e2ae2a0c
Philip Lykke Carlsen <plcplc@gmail.com>
2012-11-14 13:04:33 +0100

    (plausably) fixed vectorisation by means of better variable hygiene

--- playground/HuttonsEDSL.hs
+++ playground/HuttonsEDSL.hs
@@ -1,10 +1,17 @@
--- vim: set foldmethod=marker
+-- vim: set foldmethod=marker :
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module HuttonsEDSL where
 
-import Text.PrettyPrint
-import Unsafe.Coerce
+import Control.Applicative
+import Control.Monad.State
+
+import Data.Maybe
+import Data.Unique
+import Data.Typeable
+import Text.PrettyPrint as PP
 
 -- {{{ A minimalistic EDSL.
 -- | Expressions. More simple at the top, more specific structures below.
@@ -13,6 +20,10 @@ data Exp a where
   EInt       :: Int -> Exp Int
   EDouble    :: Double -> Exp Double
 
+  -- For arithmetic and friends...
+  -- EBinOp     :: (a -> b -> c) -> (Exp (a,b)
+
+  {-
   EPlusI     :: Exp (Int, Int) -> Exp Int
   EMultI     :: Exp (Int, Int) -> Exp Int
   EEqI       :: Exp (Int, Int) -> Exp Bool
@@ -22,81 +33,104 @@ data Exp a where
   EMultD     :: Exp (Double, Double) -> Exp Double
   EEqD       :: Exp (Double, Double) -> Exp Bool
   ELessTD    :: Exp (Double, Double) -> Exp Bool
+  -}
 
-  EArrFun    :: Exp Int -> (Exp Int -> Exp a) -> Exp (Arr a)
-  ET2        :: (Exp a, Exp b) -> Exp (a,b)
-  ET3        :: (Exp a, Exp b, Exp c) -> Exp (a,b,c)
+  EArrFun    :: Typeable a => Exp Int -> Name Int -> (Exp Int -> Exp a) -> Exp (Arr a)
+  ET2        :: (Typeable a, Typeable b) => (Exp a, Exp b) -> Exp (a,b)
+  ET3        :: (Typeable a, Typeable b, Typeable c) => (Exp a, Exp b, Exp c) -> Exp (a,b,c)
 
   -- ELam      :: (Exp a -> Exp b) -> Exp (a -> b)
   EVar       :: Name a -> Exp a
-  EApp       :: Fun a b -> Exp a -> Exp b
-  ELetIn     :: Name a -> Exp a -> (Exp a -> Exp b) -> Exp b
+  EApp       :: (Typeable a, Typeable b) => Fun a b -> Exp a -> Exp b
+  ELetIn     :: (Typeable a, Typeable b) => Name a -> Exp a -> (Exp a -> Exp b) -> Exp b
 
   -- For compositionality, all these might be Fun-wrapped in actual use.
 
-  E1of2      :: Exp (a,b) -> Exp a
-  E2of2      :: Exp (a,b) -> Exp b
-  E1of3      :: Exp (a,b,c) -> Exp a
-  E2of3      :: Exp (a,b,c) -> Exp b
-  E3of3      :: Exp (a,b,c) -> Exp c
-
+  E1of2      :: (Typeable a, Typeable b) => Exp (a,b) -> Exp a
+  E2of2      :: (Typeable a, Typeable b) => Exp (a,b) -> Exp b
+  E1of3      :: (Typeable a, Typeable b, Typeable c) => Exp (a,b,c) -> Exp a
+  E2of3      :: (Typeable a, Typeable b, Typeable c) => Exp (a,b,c) -> Exp b
+  E3of3      :: (Typeable a, Typeable b, Typeable c) => Exp (a,b,c) -> Exp c
 
   -- Note how EMapP is the only one in curried style to reflect lack of closures.
   EIf        :: Exp Bool -> Exp a -> Exp a -> Exp a
-  EMapP      :: (Exp a -> Exp b) -> Exp (Arr a) -> Exp (Arr b)
-  ECombine   :: Exp (Arr Bool, Arr a, Arr a) -> Exp (Arr a)
-  EConcat    :: Exp (Arr (Arr a)) -> Exp (Arr a)
-  EReplicate :: Exp (Int, a) -> Exp (Arr a)
-  EUnconcat  :: Exp (Arr (Arr a), Arr b) -> Exp (Arr (Arr b))
-  EZip2      :: Exp (Arr a, Arr b) -> Exp (Arr (a,b))
-  EZip3      :: Exp (Arr a, Arr b, Arr c) -> Exp (Arr (a,b,c))
-  EUnzip2    :: Exp (Arr (a,b)) -> Exp (Arr a, Arr b)
-  EIndex     :: Exp (Arr Int, Arr a) -> Exp (Arr a)
+  EMapP      :: (Typeable a, Typeable b) => Name a -> (Exp a -> Exp b) -> Exp (Arr a) -> Exp (Arr b)
+  ECombine   :: Typeable a => Exp (Arr Bool, Arr a, Arr a) -> Exp (Arr a)
+  EConcat    :: Typeable a => Exp (Arr (Arr a)) -> Exp (Arr a)
+  EReplicate :: Typeable a => Exp (Int, a) -> Exp (Arr a)
+  EUnconcat  :: (Typeable a, Typeable b) => Exp (Arr (Arr a), Arr b) -> Exp (Arr (Arr b))
+  EZip2      :: (Typeable a, Typeable b) => Exp (Arr a, Arr b) -> Exp (Arr (a,b))
+  EZip3      :: (Typeable a, Typeable b, Typeable c) => Exp (Arr a, Arr b, Arr c) -> Exp (Arr (a,b,c))
+  EUnzip2    :: (Typeable a, Typeable b) => Exp (Arr (a,b)) -> Exp (Arr a, Arr b)
+  EIndex     :: Typeable a => Exp (Arr Int, Arr a) -> Exp (Arr a)
 
   -- Bigger, temporaray building blocks
-  EReduceP   :: (Fun (a, a) a, Exp (Arr a)) -> Exp a
-  EScanP     :: (Fun (a, a) a, Exp (Arr a)) -> Exp (Arr a)
+  EReduceP   :: (Typeable a) => (Fun (a, a) a, Exp (Arr a)) -> Exp a
+  EScanP     :: (Typeable a) => (Fun (a, a) a, Exp (Arr a)) -> Exp (Arr a)
+  deriving (Typeable)
 
+data Name a where Name :: Int -> String -> Name a
+  deriving Typeable
 
 -- Only allow top-level functions by having user defined functions as a
 -- distinct type.
-data Fun a b where Fun  :: Name a -> (Exp a -> Exp b) -> Fun a b
+data Fun a b where
+  Fun  :: String -> Name a -> (Exp a -> Exp b) -> Fun a b
+  FBuiltin :: String -> (a -> b) -> Fun a b
+  deriving Typeable
 data Arr a   where Arr  :: [a] -> Arr a
-data Name a  where Name :: String -> Name a
+  deriving Typeable
 
+-- (non-fresh) construction of a variable name
 var :: String -> Name a
-var nm = Name nm
-varI :: String -> Name Int
-varI nm = Name nm
-varD :: String -> Name Double
-varD nm = Name nm
-varB :: String -> Name Bool
-varB nm = Name nm
+var nm = Name 0 nm
+
+-- }}}
+-- {{{ "StdLib"
+
+plus :: (Num a) => Fun (a,a) a
+plus = FBuiltin "+" (uncurry (+))
+(+.) x y = EApp plus (ET2(x,y))
+
+mult :: (Num a) => Fun (a,a) a
+mult = FBuiltin "*" (uncurry (*))
+x *. y = EApp mult (ET2(x,y))
 
 -- }}}
 -- {{{ Vectorisation :
--- | Lift a name to vectorised type.
-varL :: Name a -> Name (Arr a)
-varL (Name n) = Name n
 
 -- | Vectorise a function (Fun). No support for recursion yet.
-vectoriseF :: Int -> Fun a b -> Fun (Arr a) (Arr b)
-vectoriseF n (Fun argNm fn) = Fun (varL argNm) $ vectoriseArrow n argNm fn
+vectoriseF :: Typeable a =>
+              Typeable b => Int -> Fun a b -> Fresh (Fun (Arr a) (Arr b))
+vectoriseF n (Fun nm argNm fn) = do
+  Fun nm (varL argNm) <$> vectoriseArrow n fn
+vectoriseF n op@(FBuiltin nm fn) = do
+  ix <- getVar "#ix"
+  fnArg <- getVar "#fnArg"
+  return $ Fun (nm++"_L") (varL fnArg) (\v -> EMapP ix (\ix -> EApp op ix) v)
 
-vectoriseArrow :: Int -> Name a -> (Exp a -> Exp b) -> (Exp (Arr a) -> Exp (Arr b))
-vectoriseArrow n argNm fn = abstract (varL argNm) (vectoriseE n $ fn $ EVar argNm)
+vectoriseArrow :: Typeable a =>
+                  -- Typeable b => Int -> Name a -> (Exp a -> Exp b) -> Fresh (Exp (Arr a) -> Exp (Arr b))
+                  Typeable b => Int -> (Exp a -> Exp b) -> Fresh (Exp (Arr a) -> Exp (Arr b))
+--vectoriseArrow n argNm fn = abstract (varL argNm) (vectoriseE n $ fn $ EVar argNm)
+vectoriseArrow n fn = do
+  freshNm <- getVar "fresh"
+  vecBody <- vectoriseE n $ fn $ EVar freshNm
+  return $ abstract (varL freshNm) vecBody
 
 -- | Vectorise an expression (Exp). No support for function recursion yet.
-vectoriseE :: Int -> Exp a -> Exp (Arr a)
+vectoriseE :: (Typeable a) => Int -> Exp a -> Fresh (Exp (Arr a))
 vectoriseE n exp =
   case exp of
-    EBool x -> EReplicate (ET2 (EInt n, EBool x))
-    EInt x  -> EReplicate (ET2 (EInt n, EInt x))
-    EDouble x -> EReplicate (ET2 (EInt n, EDouble x))
+    EBool x -> return $ EReplicate (ET2 (EInt n, EBool x))
+    EInt x  -> return $ EReplicate (ET2 (EInt n, EInt x))
+    EDouble x -> return $ EReplicate (ET2 (EInt n, EDouble x))
 
-    ET2 (f, s) -> EZip2 $ ET2 (vectoriseE n f, vectoriseE n s)
-    EArrFun n' f -> EReplicate (ET2 (EInt n, EArrFun n' f))
+    ET2 (f, s) -> ((EZip2 . ET2) .) . (,) <$> vectoriseE n f <*> vectoriseE n s
+    ET3 (f, s, t) -> (((EZip3 . ET3) .) .) . (,,) <$> vectoriseE n f <*> vectoriseE n s <*> vectoriseE n t
+    EArrFun n' ixVar f -> return $ EReplicate (ET2 (EInt n, EArrFun n' ixVar f))
 
+{-
     EPlusI  a -> vecOp EPlusI  a
     EMultI  a -> vecOp EMultI  a
     EEqI    a -> vecOp EEqI    a
@@ -106,17 +140,26 @@ vectoriseE n exp =
     EMultD  a -> vecOp EMultD  a
     EEqD    a -> vecOp EEqD    a
     ELessTD a -> vecOp ELessTD a
+    -}
 
-    EVar x            -> EVar $ varL x
-    EApp f e          -> EApp (vectoriseF n f) (vectoriseE n e)
-    ELetIn nm val exp -> ELetIn (varL nm) (vectoriseE n val) (abstract (varL nm) (vectoriseE n $ exp (EVar nm)))
+    EVar x            -> return $ EVar $ varL x
+    EApp f e          -> EApp <$> vectoriseF n f <*> vectoriseE n e
+    ELetIn nm val exp -> do
+      vecBody <- vectoriseE n $ exp (EVar nm)
+      vecVal <- vectoriseE n val
+      return $ ELetIn (varL nm) vecVal (abstract (varL nm) vecBody)
 
     -- For compositionality, these should be Fun-wrapped in actual use.
-    EIf c t e  -> ECombine (ET3 (vectoriseE n c, vectoriseE n t, vectoriseE n e))
-    E1of2 p    -> vecOp E1of2 p
-    E2of2 p    -> vecOp E2of2 p
-    EMapP f a  -> let freshVar = var "#sharedVar"
-                  in ELetIn freshVar (vectoriseE n a) (\v -> EUnconcat $ ET2(v, vectoriseArrow n freshVar f $ EConcat v))
+    EIf c t e  -> (((ECombine . ET3) .) .) . (,,) <$> vectoriseE n c
+                                                  <*> vectoriseE n t
+                                                  <*> vectoriseE n e
+    E1of2 p    -> E1of2 . EUnzip2 <$> vectoriseE n p -- vecOp E1of2 p
+    E2of2 p    -> E2of2 . EUnzip2 <$> vectoriseE n p -- vecOp E2of2 p
+    EMapP ixVar f a  -> do
+      freshArg <- getVar "#sharedArg"
+      freshConcat <- getVar "#sharedConcat"
+      bodyExp <- vectoriseArrow n f
+      ELetIn freshArg <$> vectoriseE n a <*> pure (\v -> EUnconcat $ ET2 (v, ELetIn freshConcat (EConcat v) bodyExp))
     ECombine cte -> vecOp ECombine cte
     EConcat a    -> vecOp EConcat a
     EReplicate a -> vecOp EReplicate a
@@ -127,22 +170,43 @@ vectoriseE n exp =
     EIndex a     -> vecOp EIndex a
 
     -- Bigger, temporaray building blocks
-    EReduceP (f, a) -> EMapP (\a -> EReduceP (f,a)) $ vectoriseE n a
-    EScanP (f,a)    -> EMapP (\a -> EScanP   (f,a)) $ vectoriseE n a
+    EReduceP (f, a) -> EMapP <$> getVar "#ix" <*> pure (\a -> EReduceP (f,a)) <*> vectoriseE n a
+    EScanP (f,a)    -> EMapP <$> getVar "#ix" <*> pure (\a -> EScanP   (f,a)) <*> vectoriseE n a
   where
-    vecOp :: (Exp a -> Exp b) -> Exp a -> Exp (Arr b)
-    vecOp op arg = EMapP op $ vectoriseE n arg
+    -- Vectorise a builtin operator
+    vecOp :: (Typeable a,Typeable b) => (Exp a -> Exp b) -> Exp a -> Fresh (Exp (Arr b))
+    vecOp op arg = EMapP <$> getVar "#ix" <*> pure op <*> vectoriseE n arg
 
 -- }}}
 -- {{{ Free variable handling
+
+-- A monad for generating fresh names
+type Fresh a = State Int a
+
+runFresh :: Fresh a -> a
+runFresh act = evalState act 0
+
+getVar :: String -> Fresh (Name a)
+getVar nm = do
+  cnt <- get
+  put $ succ cnt
+  return $ Name cnt nm
+
+-- | Lift a name to vectorised type.
+varL :: Name a -> Name (Arr a)
+varL (Name i n) = Name i n
+
 -- | Abstract a free variable expression to a lambda.
-abstract :: Name a -> Exp b -> (Exp a -> Exp b)
+abstract :: Typeable a =>
+            Typeable b => Name a -> Exp b -> (Exp a -> Exp b)
 abstract nm exp = \v -> subst v nm exp
 
 -- | Perform variable substitution in expressions. Var is the only interesting case...
-subst :: Exp a -> Name a -> Exp b -> Exp b
-subst e1 nm@(Name nm') e2 =
+subst :: Typeable a =>
+         Typeable b => Exp a -> Name a -> Exp b -> Exp b
+subst e1 nm@(Name cnt nm') e2 =
   case e2 of
+{-
     EPlusI  a -> EPlusI  $ rec a
 
     EMultI  a -> EMultI  $ rec a
@@ -153,19 +217,18 @@ subst e1 nm@(Name nm') e2 =
     EMultD  a -> EMultD  $ rec a
     EEqD    a -> EEqD    $ rec a
     ELessTD a -> ELessTD $ rec a
-
-
-    EArrFun n fn -> let freeV = varI "#freshVar"
-                    in EArrFun (rec n) (abstract freeV (rec $ fn $ EVar freeV))
+-}
+    EArrFun n ixV fn -> EArrFun (rec n) ixV (abstract ixV (rec $ fn $ EVar ixV))
     ET2(a,b) -> ET2(rec a, rec b)
     ET3(a,b,c) -> ET3(rec a, rec b, rec c)
 
     -- Won't work without extensive copying or De Bruijn indexing of free variables
     -- ... So we just unsafeCoerce for now.
-    v@(EVar (Name nm'')) -> if nm' == nm'' then unsafeCoerce e1 else v
+    v@(EVar (Name cnt' nm'')) -> if nm' == nm'' && cnt == cnt'
+                                 then case cast e1 of {Just e1 -> e1; Nothing -> v}
+                                 else v
     EApp f a -> EApp f (rec a)
-    ELetIn v val exp -> let freeV = var "#freshVar"
-                        in ELetIn v (rec val) (abstract freeV (rec $ exp $ EVar freeV))
+    ELetIn v val exp -> ELetIn v (rec val) (abstract v (rec $ exp $ EVar v))
 
     -- For compositionality, all these might be Fun-wrapped in actual use.
 
@@ -177,8 +240,7 @@ subst e1 nm@(Name nm') e2 =
 
     -- Note how EMapP is the only one in curried style to reflect lack of closures.
     EIf c t e -> EIf (rec c) (rec t) (rec e)
-    EMapP f a -> let freeV = var "#freshVar"
-                 in EMapP (abstract freeV (rec $ f $ EVar freeV)) (rec a)
+    EMapP ixV f a -> EMapP ixV (abstract ixV (rec $ f $ EVar ixV)) (rec a)
     ECombine a -> ECombine $ rec a
     EConcat a  -> EConcat $ rec a
     EReplicate a -> EReplicate $ rec a
@@ -195,18 +257,29 @@ subst e1 nm@(Name nm') e2 =
     -- Constants don't do anything. Maybe would be nicer with explicit patterns to warn when extending...
     const -> const
   where
-    rec :: Exp c -> Exp c
+    rec :: Typeable c => Exp c -> Exp c
     rec x = subst e1 nm x
 
 -- }}}
-
 -- {{{ Pretty printing:
 
+-- Determine if a name should be prettyprinted infix
+infixLex [] = False
+infixLex s = (head s) `elem` "+-*/'`"
+
+
 ppFun :: Fun a b -> Doc
-ppFun (Fun nm fn) = "\\" <> ppName nm <+> "->" <+> (ppExp $ fn $ EVar nm)
+ppFun (Fun nm argNm fn) = "fun" <+> text nm <+> ppName argNm <+> "=" <+> (ppExp $ fn $ EVar argNm)
+ppFun (FBuiltin nm fn) = "fun" <+> text nm <+> "x" <+> "=" <+> "<builtin>"
+
+funName (Fun nm _ _) = nm
+funName (FBuiltin nm _) = nm
+
+ppFunName :: Fun a b -> Doc
+ppFunName f = text $ funName f
 
 ppName :: Name a -> Doc
-ppName (Name nm) = text nm
+ppName (Name cnt nm) = text $ nm  ++ if cnt > 0 then "'" ++ show cnt else ""
 
 ppExp :: Exp a -> Doc
 ppExp x = case x of
@@ -214,6 +287,7 @@ ppExp x = case x of
   EInt i    -> text $ show i
   EDouble d -> text $ show d
 
+{-
   EPlusI (ET2(a,b))  -> ppExp a <+> "+" <+> ppExp b
   EMultI (ET2(a,b))  -> ppExp a <+> "*" <+> ppExp b
   EEqI   (ET2(a,b))  -> ppExp a <+> "=" <+> ppExp b
@@ -224,23 +298,27 @@ ppExp x = case x of
   EEqD   (ET2(a,b))  -> ppExp a <+> "="  <+> ppExp b
   ELessTD (ET2(a,b)) -> ppExp a <+> "<=" <+> ppExp b
 
-  EPlusI a  -> "(+)" <+> ppExp a
-  EMultI a  -> "(*)" <+> ppExp a
-  EEqI   a  -> "(=)" <+> ppExp a
-  ELessTI a -> "(<=) " <+> ppExp a
+  EPlusI  a -> "(+)"  <+> ppExp a
+  EMultI  a -> "(*)"  <+> ppExp a
+  EEqI    a -> "(=)"  <+> ppExp a
+  ELessTI a -> "(<=)" <+> ppExp a
 
-  EPlusD a  -> "(+)" <+> ppExp a
-  EMultD a  -> "(*)" <+> ppExp a
-  EEqD   a  -> "(=)" <+> ppExp a
+  EPlusD a  -> "(+)"  <+> ppExp a
+  EMultD a  -> "(*)"  <+> ppExp a
+  EEqD   a  -> "(=)"  <+> ppExp a
   ELessTD a -> "(<=)" <+> ppExp a
+  -}
 
-  EArrFun n f -> parens $ ppExp n <+> "§" <+> (ppExp $ f $ EVar (var "$ix"))
+  EArrFun n ixV f -> parens $ ppExp n <+> "§" <+> ppName ixV <+> "->" <+> (ppExp $ f $ EVar ixV)
   ET2 (a,b)   -> parens $ ppExp a <+> "," <+> ppExp b
   ET3 (a,b,c) -> parens $ ppExp a <+> "," <+> ppExp b <+> "," <+> ppExp c
 
   EVar a -> ppName a
-  EApp f a -> "⟨fn⟩" <+> "$" <+> ppExp a
-  ELetIn nm val exp -> "let" <+> ppName nm <+> "=" <+> ppExp val $$ "in" <+> nest 2 (ppExp $ exp $ EVar nm)
+  EApp f (ET2(a,b)) | infixLex (funName f) -> ppExp a <+> ppFunName f <+> ppExp b
+  EApp f a -> ppFunName f <+> "$" <+> ppExp a
+  -- ELetIn nm val exp -> "let" <+> ppName nm <+> "=" <+> ppExp val $$ "in" <+> nest 2 (ppExp $ exp $ EVar nm)
+  letIn@(ELetIn _ _ _) -> let (docs, body) = ppLetBind ([], letIn)
+                           in "let" <+> vcat docs $$ "in" <+> (ppExp body)
 
   E1of2 x -> "#1" <+> (parens $ ppExp x)
   E2of2 x -> "#2" <+> (parens $ ppExp x)
@@ -251,7 +329,7 @@ ppExp x = case x of
 
   -- Note how EMapP is the only one in curried style to reflect lack of closures.
   EIf c t e -> "if " <+> ppExp c <+> "then" $$ (nest 2 $ ppExp t) $$ "else" $$ (nest 2 $ ppExp e)
-  EMapP f a -> "mapP" <+> nest 2 (parens (ppExp (f $ EVar $ var "$x")) $$ parens (ppExp a) )
+  EMapP x f a -> "mapP" <+> nest 2 (parens ("\\" <> ppName x <+> "->" <+> ppExp (f $ EVar x)) $$ parens (ppExp a) )
   ECombine a -> "combine" <+> parens (ppExp a)
   EConcat a -> "concat" <+> parens (ppExp a)
   EReplicate a -> "replicate" <+> parens (ppExp a)
@@ -262,8 +340,11 @@ ppExp x = case x of
   EIndex     a -> "index" <+> parens (ppExp a)
 
   -- Bigger, temporaray building blocks
-  EReduceP(f,a)-> "reduce" <+> parens ("⟨fn⟩" <+> "," <+> ppExp a)
-  EScanP  (f,a)-> "scan" <+> parens ("⟨fn⟩" <+> "," <+> ppExp a)
+  EReduceP(f,a)-> "reduce" <+> parens (ppFunName f <+> "," <+> ppExp a)
+  EScanP  (f,a)-> "scan" <+> parens (ppFunName f <+> "," <+> ppExp a)
+  where
+    ppLetBind (doc,ELetIn x val exp) = ppLetBind(doc ++ [ppName x <+> "=" <+> ppExp val], exp $ EVar x)
+    ppLetBind (doc,body) = (doc,body)
 
 
 instance (Show a, Show b) => Show (Fun a b) where
@@ -274,45 +355,39 @@ instance Show a => Show (Exp a) where
   show x = render $ ppExp x
 
 --- }}}
-
 -- {{{ Sparse matrix-vector multiplication
 
 type SparseMat = Arr (Arr (Int,Double))
 
 sparseMatMult :: Fun (SparseMat, Arr Double) (Arr Double)
 sparseMatMult =
-  Fun (var "matVec") (\matVec ->
+  Fun "sparseMatMult" (var "matVec") (\matVec ->
     ELetIn (var "mat") (E1of2 matVec)
     (\mat ->
       ELetIn (var "vec") (E2of2 matVec)
-      (\vec -> EMapP (reducePPlus)
-                     (EMapP (colProd vec) mat)
+      (\vec -> EMapP (var "red") (reducePPlus)
+                     (EMapP (var "row") (colProd vec) mat)
       ))
   )
   where
     reducePPlus ::Exp (Arr Double) -> Exp Double
-    reducePPlus x = EReduceP (plusDFn, x)
+    reducePPlus x = EReduceP (plus, x)
     colProd :: Exp (Arr Double) -> Exp (Arr (Int,Double)) -> Exp (Arr Double)
     colProd vec mat = ELetIn (var "colsVals") (EUnzip2 mat)
       (\colsVals -> ELetIn (var "cols") (E1of2 colsVals)
       (\cols -> ELetIn (var "cells") (E2of2 colsVals)
-      (\cells -> EMapP (\cellVec ->
+      (\cells -> EMapP (var "cellVec") (\cellVec ->
                          ELetIn (var "cell") (E1of2 cellVec)
                          (\cell ->
                          ELetIn (var "vec") (E2of2 cellVec)
-                         (\vec  -> EMultD $ ET2 (cell, vec)
+                         (\vec  -> cell *. vec
                          )))
                  (EZip2 $ ET2(cells, (EIndex $ ET2(cols, vec))))
       )))
 
-plusIFn :: Fun (Int, Int) Int
-plusIFn = Fun (var "ops") EPlusI
-
-plusDFn :: Fun (Double, Double) Double
-plusDFn = Fun (var "ops") EPlusD
-
 sparseMatMultVec = vectoriseF 10 sparseMatMult
 
+-- {{{ Notes
 {- Segmented array style: (using `[: [: (F,Int) :] :]` for matrix)
 
     matVecMult mat vec =
@@ -358,5 +433,5 @@ Flattening:
 
 
 -}
-
+-- }}}
 -- }}}
