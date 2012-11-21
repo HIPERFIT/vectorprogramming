26ead320b389dca0ab2f48d264a9f5c7cab59894
Philip Lykke Carlsen <plcplc@gmail.com>
2012-11-21 16:55:53 +0100

    initial somewhat working shake builder for accelerate-github

--- surveytools/setup/shakeAccelerateGithub.hs
+++ surveytools/setup/shakeAccelerateGithub.hs
@@ -4,13 +4,35 @@ import Development.Shake
 import Shake.Cabal
 import Shake.Git
 
-main = shake shakeOptions $ do
-  requireGit (GitRepo "accelerate" "git://github.com/AccelerateHS/accelerate.git")
-  requireGit (GitRepo "accelerate-cuda" "git://github.com/AccelerateHS/accelerate-cuda.git")
-  requireGit (GitRepo "accelerate" "git://github.com/AccelerateHS/accelerate-io.git")
+cudaGit  =     GitRepo "cuda" ["cuda.cabal"] "git://github.com/tmcdonell/cuda.git"
+accelGit =     GitRepo "accelerate" ["accelerate.cabal"] "git://github.com/AccelerateHS/accelerate.git"
+accelIoGit =   GitRepo "accelerate-io" ["accelerate-io.cabal"] "git://github.com/AccelerateHS/accelerate-io.git"
+accelCudaGit = GitRepo "accelerate-cuda" ["accelerate-cuda.cabal"] "git://github.com/AccelerateHS/accelerate-cuda.git"
 
-  requireCabal (CabalFile "accelerate/accelerate.cabal" [])
-  requireCabal (CabalFile "accelerate-io/accelerate-io.cabal" [])
-  requireCabal (CabalFile "accelerate-cuda/accelerate-cabal.cabal"
-    [ "--extra-include-dirs=/usr/local/cuda/include/",
-      "--extra-lib-dirs=/usr/local/cuda/lib64/"])
+gitRepos = [cudaGit, accelGit, accelIoGit, accelCudaGit]
+
+cudaCabal =      CabalFile "cuda/cuda.cabal" "cuda" [ "--extra-include-dirs=/usr/local/cuda/include/"]
+accelCabal =     CabalFile "accelerate/accelerate.cabal" "accelerate" []
+accelIoCabal =   CabalFile "accelerate-io/accelerate-io.cabal" "accelerate-io" []
+accelCudaCabal = CabalFile "accelerate-cuda/accelerate-cuda.cabal" "accelerate-cuda"
+                  ["--extra-include-dirs=/usr/local/cuda/include/",
+                    "--extra-lib-dirs=/usr/local/cuda/lib64/"]
+
+cabalPkgs = [cudaCabal, accelCabal, accelIoCabal, accelCudaCabal]
+
+main = shake shakeOptions $ do
+  gitUpdateRule
+  mapM gitFilesRule gitRepos
+
+  buildCabalRule [accelCabal, accelIoCabal, accelCudaCabal]
+  cudaCabal `hsPkgRule` (\pkg -> do
+                                  systemCwd "cuda" "autoconf" []
+                                  buildCabal pkg
+                                  )
+  action $ do
+    need $ map cabalFile cabalPkgs
+    -- requireCabal cabalPkgs -- insufficient, as the order does indeed matter..
+    requireCabal [cudaCabal]
+    requireCabal [accelCabal]
+    requireCabal [accelIoCabal]
+    requireCabal [accelCudaCabal]
--- surveytools/setup/shakelib/src/Shake/Git.hs
+++ surveytools/setup/shakelib/src/Shake/Git.hs
@@ -1,22 +1,81 @@
+ {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Shake.Git where
 
+import Control.DeepSeq
+import Control.DeepSeq.Generics
 import Control.Monad
+import Control.Monad.IO.Class
+
+import Data.Binary
+import Data.Binary.Generic
+
+import Data.Data
+import Data.Generics as DG
+
+import GHC.Generics as GG
+
+import Data.Hashable.Generic
+import Data.Typeable
 
 import Development.Shake
 import Development.Shake.FilePath
--- import System.Process
+
+import System.Exit
+import System.Process
 
 type Url = String
 
-data GitRepo = GitRepo FilePath Url
- -- deriving stuff
+-- | A Git repository is described @GitRepo localRepoDir expectedFiles remoteUrl@.
+data GitRepo = GitRepo FilePath [FilePath] Url
+  deriving (Data, Eq, GG.Generic, Show, Typeable)
 
-requireGit :: GitRepo -> Rules ()
-requireGit (GitRepo fp url) = do
-  fp *> ( \_ -> do
-    alwaysRerun
-    repoDirExists <- doesFileExist $ takeDirectory fp
-    when (not repoDirExists) $ do
-      systemCwd (takeDirectory fp) "git" ["clone", url]
+instance NFData GitRepo where
+  rnf = genericRnf
+
+instance Binary GitRepo where
+  put = putGeneric
+  get = getGeneric
+
+instance Hashable GitRepo where
+  hashWithSalt = gHashWithSalt
+
+instance Rule GitRepo () where
+  validStored (GitRepo _ _ _) time = do
+    -- Git repositories older than an hour are no longer valid
+    -- (future work)
+    return False
+
+gitUpdateRule :: Rules ()
+gitUpdateRule  = do
+  -- if anything is required inside a git repo, assume the repo contains it
+  rule $ \(GitRepo fp _ url) -> Just $ do
+    repoDirExists <- systemTest $ "test -d " ++ fp </> ".git"
+    when (not repoDirExists) (do
+      systemCwd (takeDirectory fp) "git" ["clone", url, fp])
     systemCwd fp "git" ["pull"]
-    )
+
+-- | Add shake rules for all the expected files in a git repo to require the gitUpdate
+gitFilesRule :: GitRepo -> Rules ()
+gitFilesRule (repo@(GitRepo repoDir fs _)) =
+  (map (repoDir </>) fs) **> ( \_ -> requireGit [repo])
+
+-- | State that a repository is required
+requireGit :: [GitRepo] -> Action ()
+requireGit repos = do
+  apply repos
+  return ()
+  -- (map (\ (GitRepo _ fs _) -> fs) repos)
+  --
+
+-- Run system command, and return True if ExitSuccess, False otherwise
+systemTest :: MonadIO m => String -> m Bool
+systemTest cmd = liftIO $ do
+  res <- system cmd
+  case res of
+    ExitSuccess -> return True
+    _ -> return False
