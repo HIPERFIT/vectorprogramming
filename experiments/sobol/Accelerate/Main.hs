507075fd936d9fd11fc7de3c03993aea64b74c15
Martin Dybdal <dybber@dybber.dk>
2012-12-05 16:08:29 +0100

    Sobol sequence generation in Accelerate

--- experiments/sobol/Accelerate/Main.hs
+++ experiments/sobol/Accelerate/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import Sobol
 import BenchmarkRunner.Main(runTest)
 
-main = runTest sobolSequence
+import Data.Array.Accelerate.CUDA (run)
+import Data.Array.Accelerate (constant, toList)
+
+main = runTest (\n -> toList . run . sobolInd $ constant n)
--- experiments/sobol/Accelerate/Sobol.hs
+++ experiments/sobol/Accelerate/Sobol.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeOperators #-}
+
 -- ^
 -- Sobol sequence generation in Nikola
 -- Outset from the Haskell version of the LexiFi code
@@ -6,11 +8,11 @@ module Sobol where
 import System.Environment
 
 import qualified Data.Word as DW
-import qualified Data.Bits as DB
+import Data.Bits hiding (shiftR, testBit)
 import qualified Data.List as DL
 import qualified Data.Vector as VB
 
-import Prelude hiding (map, filter, fromIntegral, zipWith)
+import Prelude hiding (map, filter, fromIntegral, zipWith, replicate, length, zip, fst, snd)
 import qualified Prelude
 
 import Data.Array.Accelerate hiding (Elem)
@@ -21,32 +23,68 @@ type Index = Int
 
 sobol_bit_count = 30
 sobol_dim = 1
-sobol_divisor = fromIntegral (2^30)
+sobol_divisor :: Exp SpecReal
+sobol_divisor = Prelude.fromIntegral (2^30)
 sobol_dirVs = [ [2^k | k <- [29,28..0]]]
 
-sobol_dirVs_array :: Acc (Array DIM2 Elem)
-sobol_dirVs_array = 
-  use $ fromList (Z :. length sobol_dirVs :. length (head sobol_dirVs)) 
-      $ Prelude.concat sobol_dirVs
+sobol_dirVs_array :: Array DIM2 Elem
+sobol_dirVs_array = fromList (Z :. Prelude.length sobol_dirVs :. Prelude.length (head sobol_dirVs)) 
+                    $ Prelude.concat sobol_dirVs
+
+length :: Shape sh => Elt a => Array (sh :. Int) a -> Int
+length arr = let (_ :. n) = arrayShape arr in n
 
 grayCode :: Exp Index -> Exp Elem
 grayCode n = fromIntegral (n `xor` (n `shiftR` 1))
 
--- Everything seems to work out except for the filter used below.
--- Can we avoid it? gatherIf?
--- Also, the use of replicate to construct a large index map for
--- gather seems a wasteful.
 sobolInd_ :: Exp Index -> Acc (Vector Elem)
-sobolInd_ n = 
-  let indices :: Acc (Vector Index)
-      indices = filter (testBit (grayCode n) . fromIntegral) $
-                use (fromList (Z :. sobol_bit_count) [0..sobol_bit_count-1])
-      indices' = replicate (Z :. length sobol_dirVs_array :. All) indices
-      arr = gather indices sobol_dir_Vs_array
-  in fold xor 0 arr
+sobolInd_ n =
+  let 
+    indices :: Acc (Array DIM2 Index)
+    indices = generate (constant $ arrayShape sobol_dirVs_array) 
+                       (fst . unindex2)
+    
+    -- The "42" constant is added as placeholder, as the operator
+    -- needs to be associative
+    doit :: Exp (Elem, Index) -> Exp (Elem, Index) -> Exp (Elem, Index)
+    doit b xi =
+      let b',x :: Exp Elem
+          i :: Exp Index
+          b' = fst b
+          x = fst xi
+          i = snd xi
+      in testBit (grayCode n) i ? (lift (x `xor` b',constant 42), lift (b', constant 42))
 
+  in map fst $ fold doit (constant (0,42)) $ zip (use sobol_dirVs_array) indices
+
+
+-- Compiles
 sobolInd :: Exp Index -> Acc (Vector SpecReal)
 sobolInd n = map norm (sobolInd_ n)
     where
-      norm :: Exp Index -> Exp SpecReal
-      norm = ( / constant sobol_divisor) . fromIntegral
+      norm :: Exp Elem -> Exp SpecReal
+      norm = ( / sobol_divisor) . fromIntegral
+
+
+-- -- Previous attempt at generating sobol sequences
+-- -- Can not go through because we cannot define the value "sifeAfterFilter"
+-- sobolInd_ :: Exp Index -> Acc (Vector Elem)
+-- sobolInd_ n = 
+--   let indices :: Acc (Vector Index)
+--       indices = filter (testBit (grayCode n) . fromIntegral) $
+--                 use (fromList (Z :. sobol_bit_count) [0..sobol_bit_count-1])
+                
+--       indices' :: Acc (Array DIM2 Index)
+--       indices' = replicate (constant $ Z :. length sobol_dirVs_array :. All) indices
+      
+--       arr :: Acc (Vector Elem)
+--       arr = gather (flatten indices') (flatten (use sobol_dirVs_array))
+      
+--       -- Seems hard to acquire this.
+--       sizeAfterFilter :: Int
+--       sizeAfterFilter = undefined
+      
+--       arr' :: Acc (Array DIM2 Elem)
+--       arr' = reshape (constant $ Z :. length sobol_dirVs_array :. sizeAfterFilter) arr
+      
+--   in fold xor 0 arr'
