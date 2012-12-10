fcb613a979636d002c7b9bb66cf2fafea32df787
Martin Dybdal <dybber@dybber.dk>
2012-12-10 21:40:33 +0100

    Trevors Accelerate version of the binomial pricer

--- benchmarks/binomial-gpu/Accelerate/American.hs
+++ benchmarks/binomial-gpu/Accelerate/American.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module American (binom, FloatRep) where
-import qualified Data.Array.Accelerate as A
-import Data.List(foldl')
+import Data.Array.Accelerate as A
+import Prelude               as P
+import Data.List(foldl1')
 
 -- Pointwise manipulation of vectors an scalars
+infixl 7 ^*^
+infixl 6 ^+^
 v1 ^*^ v2 = A.zipWith (*) v1 v2
 v1 ^+^ v2 = A.zipWith (+) v1 v2
+
+infixl 7 *^
+infixl 6 -^
 c -^ v = A.map (c -) v
 c *^ v = A.map (c *) v
 
 pmax v c = A.map (A.max c) v
 ppmax = A.zipWith A.max
 
-vtail = A.tail
-vinit = A.init
 vtake i v = A.take (A.constant i) v
 vdrop i v = A.drop (A.constant i) v
 
@@ -23,43 +27,55 @@ type FloatRep = Float
                           -- and I just compute NaN
 
 
-binom :: Int -> A.Acc(A.Vector FloatRep)
-binom expiry = first --(first ! (A.constant 0))
+binom :: Int -> Acc (Vector FloatRep)
+binom expiry = first
   where
-    uPow, dPow :: A.Acc(A.Vector FloatRep)
-    uPow = generateWithPow (A.index1 . A.constant $ n+1) (A.constant u)
-    dPow = A.reverse $ generateWithPow (A.index1 . A.constant $ n+1) (A.constant d)
-    
-    generateWithPow :: A.Exp A.DIM1 -> A.Exp FloatRep -> A.Acc (A.Vector FloatRep)
-    generateWithPow m x = A.generate m $ \ix -> let i = A.unindex1 ix in x** (A.fromIntegral i)
+    -- Actually don't want to share these definitions: they are relatively
+    -- cheap so by parameterising by 'i' we effectively "force inline" into each
+    -- put step.
+    --
+    -- Furthermore, this improves caching of the generated CUDA code kernels.
+    --
+    uPow, dPow :: Int -> Acc (Vector FloatRep)
+    dPow i = A.drop (A.constant (n+1-i))
+           $ A.reverse
+           $ A.generate (A.constant (Z:.n+1)) (\ix -> d' ** A.fromIntegral (A.unindex1 ix))
 
-    st = s0 *^ (uPow ^*^ dPow)
+    uPow i = A.take (A.constant i)
+           $ A.generate (A.constant (Z:.n+1)) (\ix -> u' ** A.fromIntegral (A.unindex1 ix))
+
+
+    -- The (>->) here instead of (.) further forces us to not share the
+    -- definitions in uPow and dPow when sequencing the puts.
+    --
+    first = foldl1' (>->) (P.map prevPut [n, n-1 .. 1]) finalPut
+
+    prevPut :: Int -> Acc (Vector FloatRep) -> Acc (Vector FloatRep)
+    prevPut i prev = ppmax (strike -^ st) (qUR *^ A.tail prev ^+^ qDR *^ A.init prev)
+      where
+        st = s0 *^ (uPow i ^*^ dPow i)
+
     finalPut = pmax (strike -^ st) 0
-
--- for (i in n:1) {
--- St<-S0*u.pow[1:i]*d.pow[i:1]
--- put[1:i]<-pmax(strike-St,(qUR*put[2:(i+1)]+qDR*put[1:i]))
--- }
-    first = foldl' prevPut finalPut [n, n-1 .. 1]
-    prevPut :: A.Acc(A.Vector FloatRep) -> Int -> A.Acc(A.Vector FloatRep)
-    prevPut put i =
-      ppmax(strike -^ st) ((qUR *^ vtail put) ^+^ (qDR *^ vinit put))
-        where st = s0 *^ ((vtake i uPow) ^*^ (vdrop (n+1-i) dPow))
+      where
+        st = s0 *^ (uPow (n+1) ^*^ dPow (n+1))
 
     -- standard econ parameters
-    strike = 100
-    bankDays :: Int
-    bankDays = 256
-    s0 = 100
-    r = 0.03; alpha = 0.07; sigma = 0.20
+    strike      = 100
+    bankDays    = 256
+    s0          = 100
+    r           = 0.03
+    alpha       = 0.07
+    sigma       = 0.20  :: FloatRep
 
-    n :: Int
-    n = expiry*bankDays
+    n           = expiry*bankDays
+    dt          = P.fromIntegral expiry / P.fromIntegral n
+    u           = exp(alpha*dt+sigma*sqrt dt)
+    d           = exp(alpha*dt-sigma*sqrt dt)
+    stepR       = exp(r*dt)
+    q           = (stepR-d)/(u-d)
+    qUR         = A.constant $ q/stepR
+    qDR         = A.constant $ (1-q)/stepR
 
-    dt, u, d :: FloatRep
-    dt = fromIntegral expiry/fromIntegral n
-    u = exp(alpha*dt+sigma*sqrt dt)
-    d = exp(alpha*dt-sigma*sqrt dt)
-    stepR = exp(r*dt)
-    q = (stepR-d)/(u-d)
-    qUR = A.constant$ q/stepR; qDR = A.constant$ (1-q)/stepR
+    u'          = A.constant u
+    d'          = A.constant d
+
