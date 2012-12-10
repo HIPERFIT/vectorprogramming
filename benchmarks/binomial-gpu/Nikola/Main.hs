1aa36bba75053b5939d54b09b070f9c1761a31a8
Philip Lykke Carlsen <plcplc@gmail.com>
2012-12-10 21:21:03 +0100

    debugged nikola

--- benchmarks/binomial-gpu/Nikola/Main.hs
+++ benchmarks/binomial-gpu/Nikola/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
+import qualified Data.Array.Nikola.Backend.CUDA as C
 import American
 import BenchmarkRunner.Main(runTestWith, cfgModSummaryFile)
 
 main = do
+  C.initializeCUDACtx
   kernelsUnComp `seq` runTestWith (cfgModSummaryFile (++ "-Uncompiled")) (binomRun kernelsUnComp)
   kernelsComp `seq` runTestWith (cfgModSummaryFile (++ "-Precompiled")) (binomRun kernelsComp)
   where kernelsUnComp = binom defaultModel
