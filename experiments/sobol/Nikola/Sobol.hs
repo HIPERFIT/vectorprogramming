cc5dfe5c30365b77f7c12454eb85642823789f4b
Martin Dybdal <dybber@dybber.dk>
2012-12-11 17:15:32 +0100

    Sobol sequence generation in Nikola - it still needs implementations of fold, xor, shiftR and testBit

--- experiments/sobol/Nikola/Sobol.hs
+++ experiments/sobol/Nikola/Sobol.hs
@@ -1,47 +1,80 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RebindableSyntax #-}
 -- ^
 -- Sobol sequence generation in Nikola
 -- Outset from the Haskell version of the LexiFi code
-module Sobol (sobolSequences) where
+module Sobol where
 
 import System.Environment
-
-import Prelude hiding (map, foldl, filter, zipWith)
+import Prelude hiding (map, filter, zipWith, zip)
 import qualified Prelude
+import Data.Word as DW
+import Data.Int
+import Data.Array.Nikola.Backend.CUDA hiding ((^))
 
-import Data.Array.Nikola.Backend.CUDA
-
--- type Elem = DW.Word32
--- type SpecReal = Double
+type Elem = DW.Word32
+type SpecReal = Double
 type Index = Int32
 
 sobol_bit_count = 30
 sobol_dim = 1
-sobol_divisor = fromIntegral (2^30)
-sobol_dirVs = [VB.fromList [2^k | k <- [29,28..0]]]
+sobol_divisor :: Exp SpecReal
+sobol_divisor = 2^30
+--sobol_dirVs = [[2^k | k <- [29,28..0]]]
 
---- Nikola does not have shifts or xor, for now we just use operators
---- with the right types
+sobol_dirVs_array :: Array D DIM2 (Exp Elem)
+sobol_dirVs_array = fromFunction (Z :. 1 :. 30)
+                                 (\(Z :. i :. j) -> fromIntegral $ 2^(29-j))
+
+
+grayCode :: Exp Index -> Exp Elem
+grayCode n = ((fromIntegral n) `xor` ((fromIntegral n) `shiftR` 1))
+
+
+--- Nikola does not have shifts, xor, testBit or fold, for now we just
+--- use functions with the "correct" types (we are still a bit unsure
+--- about the type of fold)
 xor = (|*)
 shiftR = (|*)
 
-grayCode :: Exp Index -> Exp Elem
-grayCode n = fromIntegral (n `xor` (n `shiftR` 1))
+testBit :: Exp DW.Word32 -> Exp Int32 -> Exp Bool
+testBit _ _ = undefined
 
--- Here we need a way to filter and fold over a sequence, which aren't
--- supplied by Nikola. Further more the fold is nested inside a map.
--- gatherIf from Accelerate seems appropriate, but we will still lack
--- a filter
-sobolInd_ :: Index -> [Elem]
+fold :: Shape ix =>
+        Source r a =>
+        IsArray r a =>
+        IsElem a => 
+     (a -> a -> a) -> a -> Array r (ix :. Exp Ix) a -> Array D ix a
+fold = undefined
+-------------
+
+
+sobolInd_ :: Exp Index -> Array D DIM1 (Exp Elem)
 sobolInd_ n =
-        let indices  = filter (DB.testBit (grayCode n)) [0..sobol_bit_count-1]
-            xorVs vs = DL.foldl' DB.xor 0 [vs VB.! i | i <- indices]
-        in map xorVs sobol_dirVs
+  let 
+    indices :: Array D DIM2 (Exp Index)
+    indices = fromFunction (extent sobol_dirVs_array)
+                           (\(Z :. i :. j) -> j)
+    ps :: Array D DIM2 (Exp Bool)
+    ps = map (testBit (grayCode n)) indices
 
--- Should work in Nikola
-sobolInd :: Index -> [SpecReal]
+    doit :: (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool)
+    doit a@(xa,ia) b@(xb,ib) =
+         if ia then if ib then (lift $ xa `xor` xb, lift ia)
+                    else a
+         else b
+              
+    z :: Array D DIM2 (Exp Elem, Exp Bool)
+    z = zip sobol_dirVs_array ps
+    
+    f :: Array D DIM1 (Exp Elem, Exp Bool)
+    f = fold doit (lift (0 :: Elem), lift True) z
+  in map fst f
+
+
+sobolInd :: Exp Index -> Array D DIM1 (Exp SpecReal)
 sobolInd n = map norm (sobolInd_ n)
-    where norm = ( / sobol_divisor ) . fromIntegral
-
--- This would require nested loops or perhaps flattening by hand
-sobolSequences :: Index -> [[SpecReal]]
-sobolSequences num_iters = map sobolInd [1..num_iters]
+    where
+      norm :: Exp Elem -> Exp SpecReal
+      norm = ( / sobol_divisor) . fromIntegral
