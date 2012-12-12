6ecc00b03a476b0a769f79d910997b03b1f3d7dd
Martin Dybdal <dybber@dybber.dk>
2012-12-12 15:16:52 +0100

    Now working: Sobol sequence generation in Nikola

--- experiments/sobol/Nikola/Main.hs
+++ experiments/sobol/Nikola/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
 import Sobol
-import BenchmarkRunner.Main(runTest)
+--import BenchmarkRunner.Main(runTest)
 
-main = runTest sobolSequence
+import qualified Data.Vector.CUDA.Storable as CV
+import qualified Data.Array.Nikola.Backend.CUDA.Haskell as NH
+import Data.Array.Nikola.Backend.CUDA (initializeCUDACtx)
+import Data.Int
+
+sobolSequence :: Int32 -> [[SpecReal]]
+sobolSequence n = map (CV.toList . sobol) [0..n-1]
+  where
+    sobol :: Int32 -> CV.Vector SpecReal
+    sobol = NH.compile sobolInd
+
+main = do
+  initializeCUDACtx
+  print $ sobolSequence 20
+
+--main = runTest sobolSequence
--- experiments/sobol/Nikola/Sobol.hs
+++ experiments/sobol/Nikola/Sobol.hs
@@ -7,11 +7,14 @@
 module Sobol where
 
 import System.Environment
-import Prelude hiding (map, filter, zipWith, zip)
+import Prelude hiding (map, filter, zipWith, zip, iterate, fromIntegral, (^))
 import qualified Prelude
 import Data.Word as DW
 import Data.Int
-import Data.Array.Nikola.Backend.CUDA hiding ((^))
+import Data.Typeable
+
+import Data.Array.Nikola.Backend.CUDA
+import Data.Array.Nikola.Combinators
 
 type Elem = DW.Word32
 type SpecReal = Double
@@ -20,35 +23,36 @@ type Index = Int32
 sobol_bit_count = 30
 sobol_dim = 1
 sobol_divisor :: Exp SpecReal
-sobol_divisor = 2^30
+sobol_divisor = fromInt $ bit 30
 --sobol_dirVs = [[2^k | k <- [29,28..0]]]
 
 sobol_dirVs_array :: Array D DIM2 (Exp Elem)
 sobol_dirVs_array = fromFunction (Z :. 1 :. 30)
-                                 (\(Z :. i :. j) -> fromIntegral $ 2^(29-j))
-
+                                 (\(Z :. i :. j) -> bit (29-(fromInt j)))
 
 grayCode :: Exp Index -> Exp Elem
 grayCode n = ((fromIntegral n) `xor` ((fromIntegral n) `shiftR` 1))
 
+fromIntegral :: (IsIntegral CUDA a, IsNum CUDA b) => Exp a -> Exp b
+fromIntegral = fromInt . toInt
 
---- Nikola does not have shifts, xor, testBit or fold, for now we just
---- use functions with the "correct" types (we are still a bit unsure
---- about the type of fold)
-xor = (|*)
-shiftR = (|*)
-
-testBit :: Exp DW.Word32 -> Exp Int32 -> Exp Bool
-testBit _ _ = undefined
+bit x = 1 `shiftL` x
+x `testBit` i = (x &* bit i) /=* 0
 
 fold :: Shape ix =>
         Source r a =>
         IsArray r a =>
         IsElem a => 
+        Unlift CUDA a =>
+        IsElem (Exp (Lifted CUDA a)) =>
+        Typeable (Lifted CUDA a) =>
      (a -> a -> a) -> a -> Array r (ix :. Exp Ix) a -> Array D ix a
-fold = undefined
--------------
-
+fold f b arr = 
+  let (sh :. n, lookup) = toFunction arr
+      buildArray ix = snd $ iterate n iter (0,b)
+        where
+          iter (i,x) = (i+1, f (lookup (ix :. i)) x)
+  in fromFunction sh buildArray
 
 sobolInd_ :: Exp Index -> Array D DIM1 (Exp Elem)
 sobolInd_ n =
@@ -57,7 +61,7 @@ sobolInd_ n =
     indices = fromFunction (extent sobol_dirVs_array)
                            (\(Z :. i :. j) -> j)
     ps :: Array D DIM2 (Exp Bool)
-    ps = map (testBit (grayCode n)) indices
+    ps = map (testBit (fromIntegral $ grayCode n)) indices
 
     doit :: (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool)
     doit a@(xa,ia) b@(xb,ib) =
