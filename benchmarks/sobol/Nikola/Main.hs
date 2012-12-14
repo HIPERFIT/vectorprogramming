1cad5b8f72d66783cf9f3ba9f523bf500b503fd3
Martin Dybdal <dybber@dybber.dk>
2012-12-14 12:01:46 +0100

    Make the Nikola sobol sequence generator do real benchmarking

--- benchmarks/sobol/Nikola/Main.hs
+++ benchmarks/sobol/Nikola/Main.hs
@@ -1,21 +1,28 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
 import Sobol
---import BenchmarkRunner.Main(runTest)
+import BenchmarkRunner.Main
 
 import qualified Data.Vector.CUDA.Storable as CV
 import qualified Data.Array.Nikola.Backend.CUDA.Haskell as NH
+import qualified Data.Array.Nikola.Backend.CUDA.TH as NTH
 import Data.Array.Nikola.Backend.CUDA (initializeCUDACtx)
 import Data.Int
 
-sobolSequence :: Int32 -> [[SpecReal]]
-sobolSequence n = map (CV.toList . sobol) [0..n-1]
-  where
-    sobol :: Int32 -> CV.Vector SpecReal
-    sobol = NH.compile sobolInd
+sobolIndRuntimeCompiled :: Int -> CV.Vector SpecReal
+sobolIndRuntimeCompiled = NH.compile sobolInd . Prelude.fromIntegral
+
+sobolIndPrecompiled :: Int32 -> CV.Vector SpecReal
+sobolIndPrecompiled = $(NTH.compileSig sobolInd (undefined :: Int32 -> CV.Vector SpecReal))
+
+sobolSequence :: (Int -> CV.Vector SpecReal) -> Int -> [[SpecReal]]
+sobolSequence sobol n = map (CV.toList . sobol) [0..n-1]
 
 main = do
   initializeCUDACtx
-  print $ sobolSequence 20
-
---main = runTest sobolSequence
+  sobolIndRuntimeCompiled `seq` runTestWith (cfgModSummaryFile (++ "-Uncompiled")) (sobolSequence sobolIndRuntimeCompiled)
+  sobolIndPrecompiled `seq` runTestWith (cfgModSummaryFile (++ "-Precompiled")) (sobolSequence (sobolIndPrecompiled . Prelude.fromIntegral))
+  -- runTest sobolSequence
+  -- runTest sobolSequenceCompiled
