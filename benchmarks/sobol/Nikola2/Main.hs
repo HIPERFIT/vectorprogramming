bc38fb5172845c22036de0cd8d82477007f5368d
Martin Dybdal <dybber@dybber.dk>
2012-12-14 12:00:38 +0100

    Added manually flattened version of Nikola sobol-sequence generator

---
+++ benchmarks/sobol/Nikola2/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+import Sobol
+import BenchmarkRunner.Main
+
+import qualified Data.Vector.CUDA.Storable as CV
+import Data.Array.Nikola.Backend.CUDA hiding (map, (++))
+import qualified Data.Array.Nikola.Backend.CUDA.Haskell as NH
+import qualified Data.Array.Nikola.Backend.CUDA.TH as NTH
+import Data.Array.Nikola.Backend.CUDA (initializeCUDACtx)
+import Data.Int
+
+-- Would be nice to make the length parameter unnecessary here. We
+-- haven't found away around that yet.
+sobolIndRuntimeCompiled :: Int32 -> CV.Vector Int32 -> CV.Vector SpecReal
+sobolIndRuntimeCompiled = NH.compile $ mapsobolIndr
+
+
+-- sobolIndPrecompiled :: CV.Vector Int32 -> CV.Vector SpecReal
+-- sobolIndPrecompiled = $(NTH.compileSig mapsobolInd (undefined :: CV.Vector Int32 -> CV.Vector SpecReal))
+
+sobolSequence :: (Int32 -> CV.Vector Int32 -> CV.Vector SpecReal) -> Int -> [SpecReal]
+sobolSequence sobol n = CV.toList . sobol (Prelude.fromIntegral n) . CV.fromList $ map Prelude.fromIntegral [0..n-1]
+
+main = do
+  initializeCUDACtx
+  sobolIndRuntimeCompiled `seq` runTestWith (cfgModSummaryFile (++ "-Uncompiled")) (sobolSequence sobolIndRuntimeCompiled)
+--  sobolIndPrecompiled `seq` runTestWith (cfgModSummaryFile (++ "-Precompiled")) (sobolSequence sobolIndPrecompiled)
+  -- runTest sobolSequence
+  -- runTest sobolSequenceCompiled
---
+++ benchmarks/sobol/Nikola2/Sobol.hs
@@ -0,0 +1,140 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RebindableSyntax #-}
+-- ^
+-- Sobol sequence generation in Nikola
+-- Outset from the Haskell version of the LexiFi code
+module Sobol where
+
+import System.Environment
+import Prelude hiding (map, filter, zipWith, zip, iterate, fromIntegral, (^), length, replicate)
+import qualified Prelude
+import Data.Word as DW
+import Data.Int
+import Data.Typeable
+
+import Data.Array.Nikola.Backend.CUDA
+import Data.Array.Nikola.Combinators
+
+type Elem = DW.Word32
+type SpecReal = Double
+type Index = Int32
+
+sobol_bit_count = 30
+sobol_dim = 1
+sobol_divisor :: Exp SpecReal
+sobol_divisor = fromInt $ bit 30
+--sobol_dirVs = [[2^k | k <- [29,28..0]]]
+
+sobol_dirVs_array :: Array D DIM2 (Exp Elem)
+sobol_dirVs_array = fromFunction (Z :. 1 :. 30)
+                                 (\(Z :. i :. j) -> bit (29-(fromInt j)))
+
+grayCode :: Exp Index -> Exp Elem
+grayCode n = ((fromIntegral n) `xor` ((fromIntegral n) `shiftR` 1))
+
+fromIntegral :: (IsIntegral CUDA a, IsNum CUDA b) => Exp a -> Exp b
+fromIntegral = fromInt . toInt
+
+bit x = 1 `shiftL` x
+x `testBit` i = (x &* bit i) /=* 0
+
+fold :: Shape ix =>
+        Source r a =>
+        IsArray r a =>
+        IsElem a => 
+        Unlift CUDA a =>
+        IsElem (Exp (Lifted CUDA a)) =>
+        Typeable (Lifted CUDA a) =>
+     (a -> a -> a) -> a -> Array r (ix :. Exp Ix) a -> Array D ix a
+fold f b arr = 
+  let (sh :. n, lookup) = toFunction arr
+      buildArray ix = snd $ iterate n iter (0,b)
+        where
+          iter (i,x) = (i+1, f (lookup (ix :. i)) x)
+  in fromFunction sh buildArray
+
+sobolInd_ :: Exp Index -> Array D DIM1 (Exp Elem)
+sobolInd_ n =
+  let 
+    indices :: Array D DIM2 (Exp Index)
+    indices = fromFunction (extent sobol_dirVs_array)
+                           (\(Z :. i :. j) -> j)
+    ps :: Array D DIM2 (Exp Bool)
+    ps = map (testBit (fromIntegral $ grayCode n)) indices
+
+    doit :: (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool)
+    doit a@(xa,ia) b@(xb,ib) =
+         if ia then if ib then (lift $ xa `xor` xb, lift ia)
+                    else a
+         else b
+              
+    z :: Array D DIM2 (Exp Elem, Exp Bool)
+    z = zip sobol_dirVs_array ps
+    
+    f :: Array D DIM1 (Exp Elem, Exp Bool)
+    f = fold doit (lift (0 :: Elem), lift True) z
+  in map fst f
+
+sobolInd :: Exp Index -> Array D DIM1 (Exp SpecReal)
+sobolInd n = map norm (sobolInd_ n)
+    where
+      norm :: Exp Elem -> Exp SpecReal
+      norm = ( / sobol_divisor) . fromIntegral
+
+length :: Shape (sh :. Exp Ix) =>  IsArray r a => Array r (sh :. Exp Ix) a -> Exp Int32
+length arr = let (_ :. n) = extent arr in toInt n
+
+
+replicateAddDIM :: Source r a => Shape sh => Exp Ix -> Array r sh a -> Array D (sh :. Exp Ix) a
+replicateAddDIM n arr =
+  let (sh, lookup) = toFunction arr
+  in fromFunction (sh :. n) $ \(sh' :. i) -> lookup sh'
+
+replicateAddFirstDIM :: Source r a => Exp Ix -> Array r DIM2 a -> Array D DIM3 a
+replicateAddFirstDIM n arr =
+  let (Z :. l :. m, lookup) = toFunction arr
+  in fromFunction (Z :. n :. l :. m) $ \(Z :. _ :. i :. j) -> lookup (Z :. i :. j)
+
+
+type DIM3 = DIM2 :. Exp Ix
+
+mapsobolInd_ :: Exp Int32 -> Array G DIM1 (Exp Index) -> Array D DIM2 (Exp Elem)
+mapsobolInd_ lengthn ns =
+  let 
+    indices :: Array D DIM2 (Exp Ix)
+    indices = fromFunction (extent sobol_dirVs_array)
+                           (\(Z :. i :. j) -> j)
+    
+    (Z :. dirVs_n :. dirVs_m) = extent sobol_dirVs_array
+    
+    indicesRep :: Array D DIM3 (Exp Index)
+    indicesRep = replicateAddFirstDIM (lengthn) indices
+
+    nss :: Array D DIM3 (Exp Index)
+    nss = replicateAddDIM dirVs_n $ replicateAddDIM dirVs_m ns
+
+    ps :: Array D DIM3 (Exp Bool)
+    ps = zipWith (testBit . fromIntegral . grayCode) nss indicesRep
+
+    dirVsRep :: Array D DIM3 (Exp Elem)
+    dirVsRep = replicateAddFirstDIM (lengthn) sobol_dirVs_array
+
+    doit :: (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool) -> (Exp Elem, Exp Bool)
+    doit a@(xa,ia) b@(xb,ib) =
+         if ia then if ib then (lift $ xa `xor` xb, lift ia)
+                    else a
+         else b
+              
+    z :: Array D DIM3 (Exp Elem, Exp Bool)
+    z = zip dirVsRep ps
+    
+    f :: Array D DIM2 (Exp Elem, Exp Bool)
+    f = fold doit (lift (0 :: Elem), lift True) z
+  in map fst f
+
+mapsobolInd :: Exp Int32 -> Array G DIM1 (Exp Index) -> Array D DIM2 (Exp SpecReal)
+mapsobolInd lengthn ns = map ((/sobol_divisor) . fromIntegral) $ mapsobolInd_ lengthn ns
+
+mapsobolIndr :: Exp Int32 -> Array G DIM1 (Exp Index) -> Array D DIM1 (Exp SpecReal)
+mapsobolIndr lengthn ns = reshape (Z :. lengthn) $ mapsobolInd lengthn ns
