3f64d83378f9585891e989bf9f80daa6d724348e
Martin Dybdal <dybber@dybber.dk>
2013-01-10 13:16:25 +0100

    Improved Vector Sobol

--- benchmarks/sobol/Vector/Sobol.hs
+++ benchmarks/sobol/Vector/Sobol.hs
@@ -3,12 +3,12 @@ module Sobol where
 import System.Environment
 
 import qualified Data.Word as DW
-import qualified Data.Bits as DB
-import qualified Data.List as DL
+import Data.Bits
 
+import Data.Vector.Unboxed
 import qualified Data.Vector as VB
-import qualified Data.Vector.Mutable as VM
-import qualified Data.Vector.Unboxed as VU
+import Prelude hiding (map, zipWith, foldl1)
+import qualified Prelude as P
 
 type Elem = DW.Word32
 type SpecReal = Double
@@ -17,40 +17,19 @@ type Index = Int
 sobol_bit_count = 30
 sobol_dim = 1
 sobol_divisor = fromIntegral (2^30)
-sobol_dirVs = [VB.fromList [2^k | k <- [29,28..0]]]
-
-
-lsb0_help ell c | (c DB..&. 1 == 0) = ell
-                | otherwise = lsb0_help (ell+1) (c `DB.shiftR` 1)
-
--- PROBLEM: min{ k | (rep n)[k] == 0}
--- lsb0 :: Index -> Index
-lsb0 n = lsb0_help 0 n
+sobol_dirVs = VB.fromList [fromList [2^k | k <- [29,28..0]]]
 
 grayCode :: Index -> Elem
-grayCode n = fromIntegral (n `DB.xor` (n `DB.shiftR` 1))
+grayCode ix = fromIntegral (ix `xor` (ix `shiftR` 1))
 
--- independent formula
--- LENGTH(RET) = LENGTH(sobol_dirVs)
-sobolInd_ :: Index -> [Elem]
-sobolInd_ n =
-        let indices  = filter (DB.testBit (grayCode n)) [0..sobol_bit_count-1]
-            xorVs vs = DL.foldl' DB.xor 0 [vs VB.! i | i <- indices]
-        in map xorVs sobol_dirVs
+bitVec :: Elem -> Vector Elem
+bitVec = unfoldrN sobol_bit_count (\e -> Just (e .&. 1, e `shiftR` 1))
 
--- -- independent formula
--- -- LENGTH(RET) = LENGTH(sobol_dirVs)
--- sobolInd_ :: Index -> [Elem]
--- sobolInd_ n =
---         let 
---           doit b (x,i) = if DB.testBit (grayCode n) i then x `DB.xor` b else b
---           xorVs vs = VB.foldl' doit 0 $ VB.zip vs (VB.fromList [0..sobol_bit_count-1])
---         in map xorVs sobol_dirVs
+sobolInd :: Vector Elem -> Index -> SpecReal
+sobolInd dirVs ix = normalise $ foldl1 xor $ zipWith (*) dirVs (bitVec $ grayCode ix)
+  where
+    normalise :: Elem -> SpecReal
+    normalise = ((/sobol_divisor ) . fromIntegral)
 
-
-sobolInd :: Index -> [SpecReal]
-sobolInd n = map norm (sobolInd_ n)
-    where norm = ( / sobol_divisor ) . fromIntegral
-
-sobolSequence :: Index -> [[SpecReal]]
-sobolSequence num_iters = map sobolInd [0..num_iters]
+sobolSequence :: Index -> VB.Vector SpecReal
+sobolSequence num_iters = VB.generate num_iters (sobolInd (sobol_dirVs VB.! 0))
