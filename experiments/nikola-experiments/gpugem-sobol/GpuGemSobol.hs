a8901922d10a01adb27dfba7aecb16da5933ac04
Philip Lykke Carlsen <plcplc@gmail.com>
2013-03-01 10:48:53 +0100

    long uncommitted changes to nikola gpu-gem-sobol experiment

--- experiments/nikola-experiments/gpugem-sobol/GpuGemSobol.hs
+++ experiments/nikola-experiments/gpugem-sobol/GpuGemSobol.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RebindableSyntax #-}
 
 module GpuGemSobol where
 
@@ -18,58 +20,59 @@ module GpuGemSobol where
  - sg_1600
  -}
 
-import Data.Int(Int32)
-import Data.Array.Nikola.Backend.CUDA hiding (mapNest)
+import Data.Word (Word32)
+import Data.Int (Int32)
+import Data.Array.Nikola.Combinators
+import Data.Array.Nikola.Backend.CUDA
 import Data.Array.Nikola.Backend.CUDA.Haskell
-import Prelude hiding (map)
+import Prelude hiding (map, zipWith, replicate, (^))
 
-greyCode :: Exp Int32 -> Exp Int32
-greyCode = undefined
+grayCode :: Exp Int32 -> Exp Int32
+grayCode n = ((fromIntegral n) `xor` ((fromIntegral n) `shiftR` 1))
 
-type Vec r = Array r (Z :. Exp Ix) Int32
+type Vec r = Array r (Z :. Exp Ix) (Exp Int32)
 
-bitVec   :: Exp Int32 -> Vec D
-bitVec = undefined
+fromBool :: Unlift CUDA (Exp a) => Num (Exp a) => Exp Bool -> Exp a
+fromBool b = if b then 1 else 0
+
+bit x = 1 `shiftL` x
+
+bitVec :: Exp Int32 -> Array D DIM1 (Exp Int32)
+bitVec e = map (fromBool . (/=* 0)) $ zipWith (&*) pow2s $ replicate 32 e
+  where
+    pow2s :: Array D DIM1 (Exp Int32)
+    pow2s = fromFunction (Z :. 32) (\(Z :. i) -> bit $ fromInt i)
 
 -- Should be an argument.
-dirv     :: Vec G
-dirv = undefined
+-- dirv     :: Vec G
+-- dirv = undefined
 
 -- @sobol dirv m p == {m*2^p sobol numbers}@
--- We can't reshape because it returns a push array :-(
-sobol :: Vec r -> Exp Int32 -> Exp Int32 -> Array PSH (Z :. Exp Ix :. Exp Ix) (Exp Int32)
-sobol dirv m p = mapNest (Z :. m)
-                     (\ m' x -> unfold (Z :. m) (\ (Z :. i) -> sobolRecGem dirv p (m'+i)) (x ! Z ) )
+sobol :: Array G DIM1 (Exp Int32) -> Exp Int32 -> Exp Int32
+               -> Array PSH (Z :. Exp Ix) (Exp Int32) --Array PSH (Z :. Exp Ix :. Exp Ix) (Exp Int32)
+sobol dirv m p = reshapePSH (Z :. m*p) $ mapNest (Z :. m)
+                     (\ m' x -> unfoldP (Z :. m) (\ (Z :. i) -> sobolRecGem dirv p (m'+i)) (x ! Z ) )
                      (map (sobolInd dirv) (iota p))
   where
     iota :: Exp Int32 -> Array D (Z :. Exp Ix) (Exp Int32)
     iota n = fromFunction (Z :. n) (toIndex $ Z :. n)
 
--- | @sobolInd dirv n == y_n@.
-sobolInd :: Vec r -> Exp Int32 -> Exp Int32
-sobolInd = undefined
+-- | @sobolInd dirv n == y_n@. (unnormalised)
+sobolInd :: Source r (Exp Int32) => Array r DIM1 (Exp Int32) -> Exp Int32 -> Exp Int32
+sobolInd dirVs ix = (fold xor 0 xs) ! Z
+  where
+    xs :: Array D DIM1 (Exp Int32)
+    xs = zipWith (*) dirVs (bitVec $ grayCode ix :: Vec D)
 
 -- | @sobolGem dirv jumpExp n y_n == y_{n+2^jumpExp}@
-sobolRecGem :: Vec r -> Exp Int32 -> Exp Int32 -> Exp Int32 -> Exp Int32
-sobolRecGem = undefined
+sobolRecGem :: Source r (Exp Int32) => Array r DIM1 (Exp Int32)  -> Exp Int32 -> Exp Int32 -> Exp Int32 -> Exp Int32
+sobolRecGem dirv jmp n yn =
+  (yn `xor` (dirv ! (Z:. jmp)))
+  `xor` (dirv ! (Z:. leastUnsetBit (n |* (1 `shiftL` jmp) - 1)))
 
--- sobolRecGem dirv jmp yn = yn `xor` (dirv ! jmp) `xor` (dirv ! leastUunsetBit(n `or` 2**jmp - 1))
---   where leastUnsetBit x = leastSetBit $ 2^32-1 `xor` x -- anyone knows of a better way to get all ones? ((-1) | 1) perhaps?
---         leastSetBit x = __ffs x
+leastUnsetBit :: Exp Int32 -> Exp Int32
+leastUnsetBit x = leastSetBit $ (bit 32 - 1) `xor` x -- anyone knows of a better way to get all ones? ((-1) | 1) perhaps?
 
-type family (:+:) a b :: *
-type instance sh :+: Z = sh
-type instance sh :+: (sh' :. Exp Ix) = (sh :. Exp Ix) :+: sh'
+leastSetBit :: IsBits a => Exp a -> Exp Int32
+leastSetBit x = 6666 -- undefined -- __ffs x
 
-mapNest ::
-  Shape sh =>
-  IsElem a =>
-  IsElem b =>
-  Source r a =>
-      sh' -> (Exp Ix -> Array r sh a -> Array r' sh' b)
-          -> Array r  (sh :. Exp Ix) a
-          -> Array r' ((sh :. Exp Ix) :+: sh')   b
-mapNest = undefined
-
-unfold :: sh -> (sh -> Exp a -> Exp a) -> Exp a -> Array PSH sh (Exp a)
-unfold = undefined
--- experiments/nikola-experiments/gpugem-sobol/Main.hs
+++ experiments/nikola-experiments/gpugem-sobol/Main.hs
@@ -1,5 +1,101 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 module Main where
 
+import Data.Array.Nikola.Backend.Main(defaultMain)
 import GpuGemSobol
 
-main = putStrLn "Hello world"
+-- There should really be a nikola debug module containing all these imports and definitions.
+
+-- For inspecting reification
+import Data.Int(Int32)
+import Data.Typeable(Typeable(..))
+import Control.Applicative
+
+import Data.Array.Nikola.Array
+import Data.Array.Nikola.Language.Check
+import Data.Array.Nikola.Language.Monad
+import Data.Array.Nikola.Language.Reify
+import Data.Array.Nikola.Language.Optimize as O
+import Data.Array.Nikola.Language.Sharing as Share
+import Data.Array.Nikola.Language.Syntax as S
+import Data.Array.Nikola.Language.Generic
+import Data.Array.Nikola.Backend.CUDA as C
+import Data.Array.Nikola.Backend.C.Codegen
+import Data.Array.Nikola.Backend.Flags
+import qualified Data.Array.Nikola.Exp as E
+import Data.Array.Nikola.Combinators
+import Text.PrettyPrint.Mainland
+
+import qualified Prelude as P
+import Prelude (($), Num(..), IO, snd, Monad(..), Show(..), Read(..), putStrLn, (.), undefined)
+import Control.Monad.Identity
+
+u = undefined
+
+-- usefull alias. we write this all the time..
+type N = C.Exp Int32
+
+runR' :: R a a -> IO a
+runR' m = snd <$> runR m defaultREnv
+
+reify' :: Reifiable a S.Exp => a -> P S.Exp
+reify' x = reify x
+
+printR :: Reifiable a S.Exp => a -> IO ()
+printR x = runR' (reify' x) >>= putStrLn . show . ppr
+
+
+-- Here starts actual debugging
+
+main = defaultMain (sobol)
+
+-- Important knowledge! : in order to compile an 'iterate(While)?', we rely on
+-- the fact that Share.detectSharing eliminates the DelayedE.
+itExp = iterate (10 :: N) ((1 ::N) + ) (0 :: N)
+
+sh10 = Z :. (10 ::N)
+
+-- With the presence of mapNest, fold doesn't have to be so complicated.
+fold3 ::
+  forall a b t r .
+  IsElem (E.Exp t Ix) =>
+  IsElem (E.Exp t a) =>
+  IsElem (E.Exp t b) =>
+  (E.Exp t a -> E.Exp t b -> E.Exp t a) -> E.Exp t a -> Array PSH (Z :. E.Exp t Ix) (E.Exp t b) -> E.Exp t a
+fold3 f a bs =
+  E.E $ S.DelayedE $ do
+    let APush sh bsM = bs
+    vTmp <- gensym "tmpIn"
+    loop <- reset $ do
+      (i,x) <- bsM
+      return $
+        bindE vTmp tau (ReturnE $ VarE vTmp)
+          $ bindE vTmp tau (ReturnE $ unE $ f (varE $ V vTmp) x) (ReturnE UnitE ) -- $ VarE vTmp)
+
+      --   | LetE Var Type Occ Exp Exp
+
+    return $ S.bindE vTmp tau (ReturnE $ unE a) ((sequentializeFor loop) `SeqE` (ReturnE $ VarE vTmp))
+    -- return $ S.LetE vTmp tau S.Many (unE a) ((sequentializeFor loop) `SeqE` (VarE vTmp))
+
+  where
+    tau :: S.Type
+    tau = S.ScalarT $ E.typeOf (undefined :: E.Exp t a)
+
+fold3Exp :: Array G (Z :. E.Exp CUDA Ix) (E.Exp CUDA Int32) -> E.Exp CUDA Int32
+fold3Exp x = fold3 (+) (1::N) (push x)-- (push $ fromFunction sh10 (toIndex sh10))
+
+unfoldExp :: E.Exp CUDA Int32 -> Array PSH　(Z :. E.Exp CUDA Ix) (E.Exp CUDA Int32)
+unfoldExp x = unfoldP (Z :. (10::N)) (\_ x -> x + 1) x
+
+-- Sequentialize the outermost for-loop.
+sequentializeFor :: S.Exp -> S.Exp
+sequentializeFor loop = runIdentity $ go ExpA loop
+  where
+    go :: Traversal AST Identity
+    go ExpA (ForE _ vs is body) = return $ ForE SeqParFor vs is body
+    go x loop = traverseFam go x loop
+
+
