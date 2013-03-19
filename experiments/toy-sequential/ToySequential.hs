9f1333744ff979ee318789409992bc4217f6e3d3
Philip Lykke Carlsen <plcplc@gmail.com>
2013-03-19 23:38:08 +0100

    parAlloc sketch

--- experiments/toy-sequential/ToySequential.hs
+++ experiments/toy-sequential/ToySequential.hs
@@ -41,7 +41,7 @@ data Exp :: * -> * where
 
 
 data ExecMode = SeqPar | Par | Seq | Unassigned
- deriving Show
+ deriving (Show, Eq)
 
 succEM Unassigned = Seq
 succEM Seq = Par
@@ -62,7 +62,7 @@ instance Ord ExecMode where
   compare Seq SeqPar = LT
   compare Par Unassigned = GT
   compare Par Seq = GT
-  compare Par Par = Eq
+  compare Par Par = EQ
   compare Par SeqPar = LT
   compare SeqPar Unassigned = GT
   compare SeqPar Seq = GT
@@ -223,42 +223,210 @@ toDouble = Const "toDouble" (fromRational . fromIntegral)
 
 -- {{{ Allocation of parallel resources
 
-type PA a = State ExecMode a
--- Remove 'sequential', and assign execution modes.
-parAlloc :: Exp a -> PA (Exp a)
-parAlloc a@(Const _ _) = put Seq >> return a
-parAlloc a@(Var _)     = return a
-parAlloc a@(Lam v f)   = parAlloc (f $ Var v) >>= return . (Lam v) . (abstract v)
-parAlloc l@(App f v)   = do
-  -- (sequential zipWith) f xs ys == zipWithS (seq f) (seq xs) (seq ys)
-  -- zipWith (sequential f) xs ys == zipWithP (seq f) (seqpar xs) (seqpar ys)
-  -- zipWith f (sequential xs) ys == zipWithSP f (seq xs) (seq ys)
-  st <- get
-  paV <- parAlloc v
-  st' <- get
-  paF <- if st' == Seq then do
-           put Par
-           parAlloc f
-           -- put SeqPar
-  return $ App paF paV
-parAlloc a@(Let n x f) = parAlloc (f $ Var n) >>= \a' -> return (Let n x (abstract n a'))
-parAlloc l@(If c t e)  = If <$> parAlloc c <*> parAlloc t <*> parAlloc e
-parAlloc a@(SeqParPrim e n f) = do
-  st <- get
-  return $ SeqParPrim st n f
-parAlloc a@(Sequential e)     = do
-  st <- get
-  put Seq
-  res <- parAlloc e
-  put st
-  return res
+-- | Allocation of parallelism proceeds in 4 stages:
+--
+-- 1. 'float parallelism inwards'. The innermost potentially parallel ops are
+-- identified, an `sequential` placed on their arguments to make them parallel.
+-- This we may do by lambda-elaborating: map == \f x -> map (sequential f) x To
+-- simplify, we only mark the lexically (?) innermost. For example in `(\f ->
+-- map f [..]) (map g)` we would have `map g` be the innermost map, but instead
+-- we mark `map f` innermost, and thus force `f` to be sequential, and through
+-- bubbling also force `map g`. this way we avoid having to spill constraints.
+--
+-- 2. 'top-down bubbling of sequential'. Appliction with `sequential` on rhs
+-- bubble into the corresponding function-expression.
+--
+-- 3. 'bottom-up bubbling of sequential'. Variables enclosed by `sequential`
+-- makes `sequential` bubble up to the application binding the affected
+-- variables. If the top-level type is a function type, we may have a surplus
+-- constraints limiting the possible uses of the expression. For now, lets just
+-- observe it and output it (by expanding to eta-long and applying sequential
+-- where constrained). (We should probably argue that step 2-3 is a fix point
+-- of `sequential-bubbling`)
+--
+-- 4. 'Allocate execution modes'. All execution modes of SeqParPrim's are
+-- according to their application-`sequential` context. This is readily
+-- available as `sequential` constraint bubbling has been resolved.
 
-runParAlloc :: Exp a -> Exp a
-runParAlloc e = evalState (parAlloc e) SeqPar
+parAlloc :: Exp a -> Exp a
+parAlloc = markExec . botUpSeq . topDownSeq . initialSeq
+
+initialSeq :: Exp a -> Exp a
+initialSeq = fst . initialSeq'
+
+-- We Assume absence of higher-order functions except for SeqParPrim
+initialSeq' :: Exp a -> (Exp a, Bool) -- Return True if we inserted Sequential
+initialSeq' e@(Const _ _) = (e,False)
+initialSeq' e@(Var v') = (e,False)
+initialSeq' (Lam v' f) =
+  let
+    (e,s) = initialSeq' (f $ Var v')
+  in (Lam v' $ abstract v' e, s)
+initialSeq' (Let n x f) =
+  let
+    (f',fs) = initialSeq' (f $ Var n)
+    (x',xs) = initialSeq' x
+  in (Let n x' $ abstract n f', fs || xs)
+
+-- Because of absence of higher order functions, all function-arguments to a
+-- SeqParPrim must not be variables (note: There is nothing prohibiting : `\f
+-- -> map ((\x->x) f)` or similar from passing this check. you are on your own
+-- if you divulge into this)
+-- initialSeq' (App (SeqParPrim _ n _) (Var v)) =
+--   error $ "higher order argument " ++ v ++ " given to " ++ n
+
+initialSeq' (App (App spp@(SeqParPrim _ "generate" _) x) f) =
+  let
+    (f', fs) = initialSeq' f
+    (x', _) = initialSeq' x
+  in if not fs then
+       (App (App spp x') (Sequential f'), True)
+     else
+       (App (App spp x') f', True)
+
+initialSeq' (App (App spp@(SeqParPrim _ "map" _) f) x) =
+  let
+    (f', fs) = initialSeq' f
+    (x', xs) = initialSeq' x
+  in if not fs && not xs then
+       (App (App spp (Sequential f')) x', True)
+     else
+       (App (App spp f') x', True)
+
+initialSeq' (App spp@(SeqParPrim _ "map" _) f) =
+  let
+    (f', fs) = initialSeq' f
+  in if not fs then
+       (App spp (Sequential f'), True)
+     else
+       (App spp f', True)
+
+-- There should also be some more specific cases for zipWith dealing with
+-- exec-mode of `xs` and `ys` in `zipWith f xs ys`.  how should (or can) we
+-- compile `zipWith f (mapSP ..) (mapP ..)` (not so important at this stage, as
+-- we just conclude that `f` should not be sequential.
+initialSeq' (App spp@(SeqParPrim _ "zipWith" _) f) =
+  let
+    (f', fs) = initialSeq' f
+  in if not fs then
+       (App spp (Sequential f'), True)
+     else
+       (App spp f', True)
+
+-- we can't fuse : `foldl f a (mapP ..)`
+-- but `foldl f a (mapSP ..)` is alright though.
+-- however, it seems that `initialSeq` is unable to distinguish :-(
+initialSeq' (App (App (App spp@(SeqParPrim _ "foldl" _) f) a) xs) =
+  let
+    (f',  fs)  = initialSeq' f
+    -- If 'a' is an array (and 'f' thereby an array function), 'f' and 'a' might undergo fusion.
+    -- But this we don't account for here though. It also depends on whether 'f' needs to index arbitrarily.
+    (a',  as)  = initialSeq' a
+  in if fs then
+    (App (App (App spp f') a') (Sequential xs), True)
+     else
+       -- 'f' is all sequential
+    let
+    (xs', xss) = initialSeq' xs
+    in (App (App (App spp f') a') xs', as||xss)
+
+initialSeq' (SeqParPrim _ n _) =
+  error $ "unapplied occurence of SeqParPrim " ++ n
+
+-- default case for Spps
+-- initialSeq' (App spp@(SeqParPrim _ _ _) l) =
+--   let (l', s) = initialSeq' l
+--   in if not s then
+--        (App spp (Sequential l), True)
+--      else
+--        (App spp l, True)
+
+initialSeq' (App f a) =
+  let
+    (f',fs) = initialSeq' f
+    (a',as) = initialSeq' a
+  in (App f' a', fs || as)
+
+initialSeq' (If c t e) =
+  let
+    (c',cs) = initialSeq' c
+    (t',ts) = initialSeq' t
+    (e',es) = initialSeq' e
+  in (If c' t' e', cs || ts || es)
+
+initialSeq' e@(Sequential _) = (e, True)
+
+topDownSeq :: Exp a -> Exp a
+topDownSeq = id -- undefined
+
+botUpSeq :: Exp a -> Exp a
+botUpSeq = id -- undefined
+
+markExec :: Exp a -> Exp a
+markExec ev@(Lam v f)  = Lam v (abstract v (markExec $ f $ Var v))
+markExec ev@(Let n y f) = Let n (markExec y) (abstract n (markExec $ f $ Var n))
+-- lots and lots of specific cases:
+markExec (App (App (SeqParPrim _ "map" spp) f) xs) =
+  let mode = case (f, xs) of
+              (Sequential _, Sequential _) -> Seq
+              (Sequential _,  _) -> Par
+              (_,  Sequential _) -> SeqPar
+              (_, _) -> SeqPar
+  in App (App (SeqParPrim mode "map" spp) (markExec f)) (markExec xs)
+
+{- Really should not occur, bubbling should make sure to be eta-long
+markExec (App (SeqParPrim _ "map" spp) f) =
+  let mode = case f of
+    Sequential _ -> Par
+    _  -> SeqPar
+  in App (SeqParPrim mode "map" spp) (markExec f)
+-}
+
+markExec (App (App (SeqParPrim _ "generate" spp) x) f) =
+  let mode = case (f, x) of
+              (Sequential _, Sequential _) -> Par
+              (Sequential _,  _) -> Par
+              (_,  Sequential _) -> SeqPar
+              (_, _) -> SeqPar
+  in App (App (SeqParPrim mode "generate" spp) (markExec x)) (markExec f)
+
+markExec (App (App (App (SeqParPrim _ "zipWith" spp) f) xs) ys) =
+  let mode = case (f, xs, ys) of
+              (Sequential _, Sequential _,            _) -> Seq
+              (Sequential _,            _, Sequential _) -> Seq
+              (           _, Sequential _,            _) -> SeqPar
+              (           _,            _, Sequential _) -> SeqPar
+              (           _,            _,            _) -> SeqPar
+  in App (App (App (SeqParPrim mode "zipWith" spp) (markExec f)) (markExec xs)) (markExec ys)
+
+-- markExec (App (App (App spp@(SeqParPrim _ "unfoldl" _) f) a) xs) = undefined
+markExec (App (App (App (SeqParPrim _ "foldl" spp) f) a) xs) =
+  let mode = case (f, xs) of
+              (Sequential _,            _) -> Seq
+              (           _, Sequential _) -> SeqPar
+              (           _,            _) -> SeqPar
+  in App (App (App (SeqParPrim mode "foldl" spp) (markExec f)) (markExec a)) (markExec xs)
+
+markExec ev@(App f a)   = App (markExec f) (markExec a)
+markExec ev@(If c t e)  = If (markExec c) (markExec t) (markExec e)
+markExec ev@(Const _ _) = ev
+markExec ev@(Var _)     = ev
+markExec ev@(Sequential e) = allSeq e
+  where
+    -- And now it would be nice to have some generics..
+    allSeq :: Exp a -> Exp a
+    allSeq (SeqParPrim _ n f) = SeqParPrim Seq n f
+    allSeq (Lam v f)   = Lam v (abstract v (allSeq $ f $ Var v))
+    allSeq (Let n y f) = Let n (allSeq y) (abstract n (allSeq $ f $ Var n))
+    allSeq (App f a)   = App (allSeq f) (allSeq a)
+    allSeq (If c t e)  = If (allSeq c) (allSeq t) (allSeq e)
+    allSeq ev@(Const _ _) = ev
+    allSeq ev@(Var _)     = ev
+    allSeq (Sequential e) = e
 
 -- Abstract a variable from a first order representation back to HOAS
 -- So, HOAS didn't save us from implementing capture-avoiding substitution
-abstract :: Typeable a => String -> Exp b -> Exp a -> Exp b
+abstract :: Typeable a => String -> Exp b -> (Exp a -> Exp b)
 abstract v ev@(Var v') | v == v' = \x -> maybe ev id (cast x)
 abstract v ev@(Lam v' _) | v == v' = \x -> ev
 abstract v ev@(Lam v' f) | v /= v' = \x -> Lam v' (\x' -> abstract v' (abstract v (f $ Var v') x) x')
