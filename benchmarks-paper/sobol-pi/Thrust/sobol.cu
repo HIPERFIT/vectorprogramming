04b64b141e5f1921b691a0e6f998bb441d5c19b5
Martin Dybdal <dybber@dybber.dk>
2013-06-15 13:26:05 +0200

    sobol-pi/Thrust is now without raw CUDA code

--- benchmarks-paper/sobol-pi/Thrust/sobol.cu
+++ benchmarks-paper/sobol-pi/Thrust/sobol.cu
@@ -1,13 +1,19 @@
+#include <iostream>
+#include <stdexcept>
+#include <math.h>
+#include <stdio.h>
+
 #include "sobol.h"
 #include <thrust/device_vector.h>
+#include <thrust/generate.h>
 
 #define TESTBIT(var,pos) (((var) & (1<<(pos))) != 0)
 #define k_2powneg32 2.3283064E-10F
 
 // Inductively generates a Sobol number from a direction vector and
 // the index in the sequence
-__host__ __device__ 
-float sobolInductive(unsigned int *dir, unsigned int i) {
+__host__ __device__
+float sobolInductive(const unsigned int *dir, unsigned int i) {
   unsigned int acc = 0;
   for(int j = 0; j < n_directions; j++) {
     acc ^= TESTBIT(i,j) * dir[j];
@@ -15,34 +21,35 @@ float sobolInductive(unsigned int *dir, unsigned int i) {
   return ((float)acc) * k_2powneg32;
 }
 
-// Generates a multidimensional Sobol-sequence where all numbers of
-// the first dimension is placed first in the output array, followed
-// by the second dimension and so on.
-__global__ 
-void cuda_sobolInductive(unsigned int *d_directions, float *d_output,
-                         unsigned int n, unsigned int dimensions) {
-  const int gridSize = __umul24(blockDim.x, gridDim.x);
+struct sobolInductiveFunctor {
+private:
+  const int n;
+  const unsigned int *d_directions_raw;
 
-  for (int i = __umul24(blockDim.x, blockIdx.x) + threadIdx.x; 
-       i < n * dimensions;
-       i += gridSize) {
+public:
+  sobolInductiveFunctor(unsigned int _n, unsigned int* _d_directions_raw)
+    : n(_n), d_directions_raw(_d_directions_raw)  {}
+
+  __host__ __device__
+  float operator()(const int& i) const {
     int dirvec_id = i / n;
     int sobol_id = i % n;
-    d_output[i] = sobolInductive(&d_directions[dirvec_id*n_directions], sobol_id);
+    return sobolInductive(&d_directions_raw[dirvec_id*n_directions], sobol_id);
   }
-}
+};
 
-// Thrust-interface to cuda_sobolInductive
 void generate_sobol(thrust::device_vector<unsigned int> *d_directions,
                     thrust::device_vector<float> *d_sobol,
                     unsigned int n,
                     unsigned int dimensions) {
-  // Convert to raw CUDA pointers
-  unsigned int *d_directions_raw = thrust::raw_pointer_cast(&(*d_directions)[0]);
-  float *d_sobol_raw = thrust::raw_pointer_cast(&(*d_sobol)[0]);
 
-  // TODO: Make an informed choice of launch parameters
-  int blockSize = 32;
-  int gridSize = 480;
-  cuda_sobolInductive<<<gridSize, blockSize>>>(d_directions_raw, d_sobol_raw, n, dimensions);
+  // Convert to raw CUDA pointer
+  unsigned int *d_directions_raw = thrust::raw_pointer_cast(&(*d_directions)[0]);
+  thrust::counting_iterator<int> iter(0);
+
+  // Should be a transform iterator
+  thrust::transform(iter,
+                    iter + n*dimensions,
+                    d_sobol->begin(),
+                    sobolInductiveFunctor(n, d_directions_raw));
 }
